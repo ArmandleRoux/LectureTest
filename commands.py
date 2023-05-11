35eff637d33507c5db6505893c3bf8cabbbb10cd
ArmandleRoux <arramband@gmail.com>
2023-05-11 20:23:33 +0200

    Added git add, status, commit and push

--- commands.py
+++ commands.py
@@ -1,3 +1,7 @@
 command = {
-    "git clone" : "Get a copy from our remote repository"
+    "git clone" : "Get a copy from our remote repository",
+    "git add [file name]" : "Add our file to staging area",
+    "git status" : "Check status of files to be commited",
+    "git commit -m message" : "Commit all changes made to repository",
+    "git push" : "Added all our chanegs to our remote repository"
 }
